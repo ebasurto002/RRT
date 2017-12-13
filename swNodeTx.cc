f86ce42756a0e4458f411a153367b934804254da
Estrella Basurto <ebasurto002@ehu.es>
2017-12-13 17:38:23 +0100

    Clase para el nodo transmisor acabada. Sin recogida de estadísticos

--- swNodeTx.cc
+++ swNodeTx.cc
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <omnetpp.h>
 #include <paquete_m.h>
+#include <algorithm>
 
 using namespace omnetpp;
 
@@ -23,6 +24,7 @@ class swNodeTx: public cSimpleModule {
             paquete *packet;
         };
         arrivals *arr;
+        cMessage *sendEvent;
     protected:
         virtual void initialize() override;
         virtual void handleMessage(cMessage *msg) override;
@@ -54,12 +56,51 @@ void swNodeTx::initialize(){
 
     numPaquete = 0;
     txdpackets = 0;
-
-    send(arr[0].packet -> dup(),"out");
-    scheduleAt(simTime()+timeout, timeoutEvent);
+    sendEvent = new cMessage("sendEvent");
+    scheduleAt(arr[0].llegadas,sendEvent);
 
 }
 void swNodeTx::handleMessage(cMessage *msg){
+
+    //Caso de que expire el temporizador
+    if(msg == timeoutEvent){
+        EV<<"Timeout expired, resending packet..." << endl;
+        send(arr[numPaquete].packet -> dup(),"out");
+        scheduleAt(simTime()+timeout,timeoutEvent);
+        txdpackets++;
+
+    }
+
+    //Caso de que se avise al nodo de que tiene un paquete para enviar
+    if(msg == sendEvent){
+        send(arr[numPaquete].packet -> dup(),"out");
+        scheduleAt(simTime()+timeout,timeoutEvent);
+        txdpackets++;
+    }
+    else{
+        //Caso de haberse perdido el ACK
+        if(uniform(0,1)<par("pErrACK").doubleValue()){
+            EV<<"ACK Lost"<<endl;
+            bubble("ACK Lost");
+        }
+        //Caso de que vaya bien
+        else{
+            EV<<"Cancelling timeout"<<endl;
+            cancelEvent(timeoutEvent);
+
+            numPaquete++;
+            if(numPaquete < (int) par("nPaquetes")){
+                //Hay que mirar si hay paquetes en la cola o aún no han llegado. Como la simulación de Stop&Wait de Mathematica
+                double time = std::max(simTime().dbl(),arr[numPaquete].llegadas);
+                scheduleAt(time,sendEvent);
+            }
+        }
+        delete msg;
+    }
+
+}
+
+void swNodeTx::finish(){
 
 }
 
